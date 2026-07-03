4652e45369a4fdff5a1313387baf7460193acd35
Jorge Sirvent <ortsinton@gmail.com>
2026-07-03 16:49:57 +0200

    Initial Commit

---
+++ EasySaving/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  EasySaving
+//
+//  Created by Jorge Sirvent on 3/7/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ EasySavingTests/EasySavingTests.swift
@@ -0,0 +1,18 @@
+//
+//  EasySavingTests.swift
+//  EasySavingTests
+//
+//  Created by Jorge Sirvent on 3/7/26.
+//
+
+import Testing
+
+struct EasySavingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
